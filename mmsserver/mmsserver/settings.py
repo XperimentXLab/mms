ab0483b1e73cd29d14b3998ed4e7ff18f3077bce
XperimentXLab <xperimentxlab@gmail.com>
2025-06-01 21:47:38 +0800

    fix db

--- mmsserver/mmsserver/settings.py
+++ mmsserver/mmsserver/settings.py
@@ -152,15 +152,12 @@ if DATABASE_URL:
 else: 
     DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DBNAME"),
-        'USER': os.getenv("USER"),
-        'PASSWORD': os.getenv("PASSWORD"),
-        'HOST': os.getenv("HOST"),
-        'PORT': os.getenv("PORT"),
-    
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
-
-
 
 
 
