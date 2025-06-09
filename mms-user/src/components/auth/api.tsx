febb9a16971064bc93fdf6863a0d2b17ebd550dc
XperimentXLab <xperimentxlab@gmail.com>
2025-06-09 14:59:46 +0800

    debug baseurl

--- mms-user/src/components/auth/api.tsx
+++ mms-user/src/components/auth/api.tsx
@@ -2,6 +2,7 @@ import axios from "axios"
 
 export const baseURL = process.env.NEXT_PUBLIC_BASE_URL ||'http://127.0.0.1:8000/server'
 const tokenURL = `${baseURL}/token/refresh/`
+console.log("Base URL:", process.env.NEXT_PUBLIC_BASE_URL);
 
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken'; // This is the default, but good to be explicit
