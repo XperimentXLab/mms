e6ed09937cb83b8834dd387c73428f595892950e
XperimentXLab <xperimentxlab@gmail.com>
2025-06-09 15:39:36 +0800

    redeploy

--- mms-user/src/components/auth/api.tsx
+++ mms-user/src/components/auth/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const baseURL = process.env.VITE_BASE_URL ?? 'http://127.0.0.1:8000/server'
+export const baseURL = import.meta.env.VITE_BASE_URL ?? 'http://127.0.0.1:8000/server'
 const tokenURL = `${baseURL}/token/refresh/`
 
 axios.defaults.xsrfCookieName = 'csrftoken';
