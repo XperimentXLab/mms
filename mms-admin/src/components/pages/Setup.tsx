2c6f65a6776abd49ae686470ae801876cb5f4524
XperimentXLab <xperimentxlab@gmail.com>
2025-06-27 23:19:26 +0800

    fix setup

--- mms-admin/src/components/pages/Setup.tsx
+++ mms-admin/src/components/pages/Setup.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { Inputss } from "../props/Formss"
 import Loading from "../props/Loading"
 import Buttons from "../props/Buttons"
