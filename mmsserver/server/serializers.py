723e95e7fcefbea2b117584c525868072e43973e
XperimentX Lab <145745558+XperimentXLab@users.noreply.github.com>
2025-06-30 11:49:47 +0800

    fix register serializer

--- mmsserver/server/serializers.py
+++ mmsserver/server/serializers.py
@@ -125,7 +125,10 @@ class UserSerializer(serializers.ModelSerializer):
     return obj.get_verification_status_display()
 
   def create(self, validated_data):
-    user = User.objects.create_user(**validated_data)
+    password = validated_data.pop('password')
+    user = User(**validated_data)
+    user.set_password(password)  # hashes the password properly
+    user.save()
     return user
 
 class UserNetworkSerializer(serializers.ModelSerializer):
