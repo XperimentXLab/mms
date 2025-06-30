00cd999d7d1d5615972d71ba2f35609003b0f7d0
XperimentX Lab <145745558+XperimentXLab@users.noreply.github.com>
2025-06-30 12:22:37 +0800

    fix register

--- mmsserver/server/serializers.py
+++ mmsserver/server/serializers.py
@@ -93,6 +93,8 @@ class UserSerializer(serializers.ModelSerializer):
     return value
   
   def validate_username(self, value):
+    if not (isinstance(value, str) and value.startswith('MMS')):
+      raise serializers.ValidationError(f'Username must start with MMS.')
     if User.objects.filter(username=value).exists():
       raise serializers.ValidationError('Username already in use')
     return value
