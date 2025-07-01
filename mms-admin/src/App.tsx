a07833dfd1ad7ec51d9152ceed4883fa2fca9c0f
XperimentX Lab <145745558+XperimentXLab@users.noreply.github.com>
2025-07-01 14:51:16 +0800

    add new App

---
+++ mms-admin/src/App.tsx
@@ -0,0 +1,43 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
+import ProtectedRoute from "./components/auth/ProtectedRoute"
+import MainLayout from "./components/layout/MainLayout"
+import Login from "./components/auth/Login"
+import Operation from "./components/pages/Operation"
+import Dashboard from "./components/pages/Dashboard"
+import Setup from "./components/pages/Setup"
+import AssetRequest from "./components/pages/AssetRequest"
+import User from "./components/pages/User"
+
+
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route>
+    <Route path="/login" element={<Login />} />
+
+    <Route element={<ProtectedRoute />} >
+      <Route element={<MainLayout />}>
+
+        <Route path="/" element={<Dashboard />}/>
+        <Route path="/setup" element={<Setup />} /> 
+        <Route path="/operation" element={<Operation />}/> 
+        <Route path="/users" element={<User />} />
+        <Route path="/verification" />
+        <Route path="/asset/requests" element={<AssetRequest />}/> 
+        <Route path="/withdraw/requests" /> 
+        <Route path="/transaction" /> 
+
+      </Route>
+
+    </Route>
+
+  </Route>
+))
+
+function App() {
+
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
