e10cc980015c1dcd1d303c999685b465d3452012
XperimentX Lab <145745558+XperimentXLab@users.noreply.github.com>
2025-07-04 09:31:41 +0800

    fixed local timezone

--- mmsserver/mmsserver/settings.py
+++ mmsserver/mmsserver/settings.py
@@ -46,6 +46,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
+TIME_ZONE = 'Asia/Kuala_Lumpur'
+USE_TZ = True
+
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 #Supabase
