195ca06f8ae94dc62a004cdba54e535ac8044ac9
XperimentX Lab <145745558+XperimentXLab@users.noreply.github.com>
2025-07-04 10:15:39 +0800

    fix home daily profit

--- mmsserver/server/views.py
+++ mmsserver/server/views.py
@@ -645,7 +645,7 @@ def get_daily_total_profit(request):
     transaction = Transaction.objects.filter(user=user, transaction_type__in=['DISTRIBUTION',
     'AFFILIATE_BONUS', 'INTRODUCER_BONUS'],
     created_at__date=today).values("transaction_type").annotate(
-      total_amount=Sum("amount")
+      total_amount=sum("amount")
     )
     serializer = TransactionSerializer(transaction, many=True)
     return Response(serializer.data, status=200)
