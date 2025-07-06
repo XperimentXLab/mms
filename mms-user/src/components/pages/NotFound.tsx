d0ac32f05b27f44832b59726cef307a419cff0dd
XperimentXLab <xperimentxlab@gmail.com>
2025-07-06 19:03:45 +0800

    fix MF

--- mms-user/src/components/pages/NotFound.tsx
+++ mms-user/src/components/pages/NotFound.tsx
@@ -3,9 +3,11 @@ import { NavLinkss } from "../props/theLinks"
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center p-10">
       <span>Sorry, pages you looking for not found!</span>
-      <NavLinkss to="/">Go Home</NavLinkss>
+      <NavLinkss to="/"
+        className="px-3 py-2 border hover:bg-black active:bg-black hover:text-white active:text-white rounded-xl"
+      >Go Home</NavLinkss>
     </div>
   )
 }
