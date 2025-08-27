636b7d9e1970064805918739bdf02651dd304d96
XperimentXLab <xperimentxlab@gmail.com>
2025-08-27 21:29:41 +0800

    mventure final draft -3

--- mventure/src/components/Join/WhoCanJoin.tsx
+++ mventure/src/components/Join/WhoCanJoin.tsx
@@ -20,17 +20,20 @@ const WhoCanJoin = () => {
       <div className="flex flex-col gap-2 bg-gradient-to-r from-indigo-300 via-purple-300 to-pink-300 p-3 transition-all shadow-md shadow-blue-800 duration-500 rounded-2xl items-center">
         <h1 className="text-2xl font-['Concert_One']">Who Can Join Us?</h1>
 
-        <span>
-          <strong className="font-['Stylish']">Individuals:</strong> For both local residents and non-residents, as long as they are 18 years or older. A low minimum investment makes it accessible to a wide audience.
-        </span>
+        <div className="flex flex-col gap-2 items-start">
+          <span>
+            <strong className="font-['Stylish']">Individuals:</strong> For both local residents and non-residents, as long as they are 18 years or older. A low minimum investment makes it accessible to a wide audience.
+          </span>
 
-        <span>
-          <strong className="font-['Stylish']">Corporate Investors:</strong> Companies and other organizations that interested invest their funds.
-        </span>
+          <span>
+            <strong className="font-['Stylish']">Corporate Investors:</strong> Companies and other organizations that interested invest their funds.
+          </span>
 
-        <span>
-          <strong className="font-['Stylish']">Investors with a Long-Term Horizon:</strong> Unit trusts are a suitable option for those who can invest for a longer period to take advantage of the compounding effect.
-        </span>
+          <span>
+            <strong className="font-['Stylish']">Investors with a Long-Term Horizon:</strong> Unit trusts are a suitable option for those who can invest for a longer period to take advantage of the compounding effect.
+          </span>
+        </div>
+
       </div>
     </Transition>
   )
