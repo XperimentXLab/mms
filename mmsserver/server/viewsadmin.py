2019ebdae5658070c417e27395fa3b38986d720b
XperimentXLab <xperimentxlab@gmail.com>
2025-10-02 18:24:00 +0800

    fix tx filter date 5

--- mmsserver/server/viewsadmin.py
+++ mmsserver/server/viewsadmin.py
@@ -390,14 +390,13 @@ def get_all_transaction(request):
       elif range_type == '3month':
         end_date = timezone.now()
         start_date = end_date - timedelta(days=90)
-      else:
+      elif range_type == None and month == None and year == None:
         # ⏱ Default date range fallback
-        if start_date and end_date:
-          query &= date_filter_q('created_at', start_date, end_date)
-        else: 
-          start_date = timezone.now() - timedelta(days=30)
-          end_date = timezone.now()
-          query &= date_filter_q('created_at', start_date, end_date)
+        start_date = timezone.now() - timedelta(days=30)
+        end_date = timezone.now()
+
+      if start_date and end_date:
+        query &= date_filter_q('created_at', start_date, end_date)
 
       transactions = Transaction.objects.filter(query).order_by('-created_at')
 
